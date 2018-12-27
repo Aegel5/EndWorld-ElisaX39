ffaca42b00827e6a81bcfb7e841bc6c35049f63e
alex <alex@alex.ru>
2018-12-27 23:43:39 +0300

    1

---
+++ Assets/scripts/PauseGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PauseGame : MonoBehaviour
+{
+    public Transform canvas;
+
+    FirstPersonController player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Pause();
+    }
+
+    public void Pause()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (canvas.gameObject.activeInHierarchy == false)
+            {
+                canvas.gameObject.SetActive(true);
+                Time.timeScale = 0;
+                player.enabled = false;
+            }
+            else
+            {
+                canvas.gameObject.SetActive(false);
+                Time.timeScale = 1;
+                player.enabled = true;
+            }
+        }
+    }
+}
---
+++ Assets/scripts/from_book/Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float HealthPoints
+    {
+        get { return healthPoints; }
+        set
+        {
+            healthPoints = value;
+            //If health is < 0 then die
+            if (healthPoints <= 0)
+                Destroy(gameObject);
+        }
+    }
+    [
+  SerializeField]
+    private float healthPoints = 100f;
+
+}
